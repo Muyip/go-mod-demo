3f77ac3011cad7d3dafd1f54f942273eee177c30
Muyip <yp_abc2015@163.com>
2023-01-11 11:14:24 +0800

    go mod init

---
+++ go-mod-root/go.mod
@@ -0,0 +1,3 @@
+module github.com/Muyip/go-mod-demo
+
+go 1.18
---
+++ go-mod-root/util/util.go
@@ -0,0 +1,15 @@
+package util
+
+// Copyright (c) 2023 Baidu.com, Inc. All Rights Reserved
+
+// Author: yangpeng(yangpeng20.baidu.com)
+// Create: 2023/01/11
+// Desc  : 
+
+import (
+	"fmt"
+)
+
+func HelloWorldYP() {
+	fmt.Println("HelloWorld")
+}
